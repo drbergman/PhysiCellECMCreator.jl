a9a07d6e4c35e3c2af66458c4d9566de7b6fce5d
Daniel Bergman <danielrbergman@gmail.com>
2025-02-10 11:03:37 -0500

    change hierarchy and introduce ellipse_with_shell
    - match hierarchy of PhysiCellCellCreator.jl
    - each layer now has `patch_collection`'s defining which types are there
    - patch elements no longer contain this info
    - add `ellipse_with_shell` to create a patch of both the ellipse and the shell
    - add to docstrings, docs, and tests

--- test/WriteECMTests.jl
+++ test/WriteECMTests.jl
@@ -1,6 +1,9 @@
 using PhysiCellECMCreator
 
-createICECMXMLTemplate("test")
-
 config_dict = Dict("x_min" => -400.0, "x_max" => 400.0, "y_min" => -400.0, "y_max" => 400.0, "dx" => 40.0, "dy" => 40.0)
+
+createICECMXMLTemplate("test")
 generateICECM("test/ecm.xml", "test.csv", config_dict)
+
+createICECMXMLTemplate("test_monolayer"; monolayer=true)
+generateICECM("test_monolayer/ecm.xml", "test_monolayer.csv", config_dict)
